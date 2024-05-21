1c6ecbf881a0326973c0a068826355c5af327361
Your Name <you@example.com>
2024-05-21 17:32:32 +0600

    payment Return model update

--- app/Models/PaymentReturn.php
+++ app/Models/PaymentReturn.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentReturn extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'flat_id',
+        'flatSale_id',
+        'payment_type',
+        'amount',
+        'bank_name',
+        'branch',
+        'account_number',
+        'check_number',
+        'status',
+
+        'received_by',
+    ];
 }
